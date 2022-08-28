3a3a3dbb47f8cd3c7bb491312a65f979518cb1e1
BMidhun <midhunbabygeorge@gmail.com>
2022-08-28 15:42:03 +0530

    rule #8

--- car-dealer-example/schema.js
+++ car-dealer-example/schema.js
@@ -7,12 +7,12 @@ const typeDefs = gql`
 
   type Car { }
 
-  // Add pagination capability to cars in a Group. skip input will skip the documents and take will take the amount of cars we need to provide. 
+  // Renaming bodyHtml to description
 
   type Group {
     id:ID!
     name:String!
-    bodyHtml:String!
+    description:String!
     image:Image!
     cars(skip:Int!, take:Int!):[Car!]!
     featureSet:GroupFeatureSet
