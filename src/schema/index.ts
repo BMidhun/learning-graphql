eae189d901e4b136ee8e699b9db79b564d5a233a
BMidhun <midhunbabygeorge@gmail.com>
2022-09-02 19:17:20 +0530

    add Queries

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -4,6 +4,29 @@ const typeDefs = gql`
  type Query {
     hello: String!
  }
+
+ type Profile {
+   id:ID!
+   bio:String!
+   user: User!
+ }
+
+ type Post {
+   id:ID!
+   title: String!
+   content:String!
+   published:Boolean!
+   createdAt:String!
+   author:User!
+ }
+
+ type User {
+   id:ID!
+   email:String!
+   name:String!
+   profile : Profile!
+   posts : [Post!]!
+ }
 `
 
 export default typeDefs;
