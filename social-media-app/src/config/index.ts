3dc99488adae66a056bc19b43c776d8b528fc2a2
BMidhun <midhunbabygeorge@gmail.com>
2022-09-05 00:31:26 +0530

    setup graphql at top level

---
+++ social-media-app/src/config/index.ts
@@ -0,0 +1,6 @@
+
+const config = {
+    serverUri : "http://localhost:4000/graphql"
+};
+
+export default config;
--- social-media-app/src/main.tsx
+++ social-media-app/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import config from "./config";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const client = new ApolloClient({
+  uri: config.serverUri,
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
-)
+);
