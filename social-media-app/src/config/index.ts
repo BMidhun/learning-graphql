af3d672a1d80ea050acebded78899fc3f349733b
BMidhun <midhunbabygeorge@gmail.com>
2022-09-05 00:58:33 +0530

    query posts to component

--- social-media-app/src/config/index.ts
+++ social-media-app/src/config/index.ts
@@ -1,6 +1,6 @@
 
 const config = {
-    serverUri : "http://localhost:4000/graphql"
+    serverUri : "http://localhost:4000/"
 };
 
 export default config;
--- social-media-app/src/pages/posts/index.tsx
+++ social-media-app/src/pages/posts/index.tsx
@@ -1,11 +1,52 @@
-import React from 'react'
+import {gql, useQuery} from "@apollo/client"
+import { useState } from "react"
+
+interface PostData {
+  id:number
+  title:string
+  content:string
+  createdAt:string
+  author:{
+    name:string
+  }
+}
+
+interface QueryInput {
+  skip:number,
+  take:number
+}
+
+const GET_POST = gql`
+  query($take:Int!, $skip:Int!){
+    posts(take: $take, skip:$skip){
+      id
+      title
+      content
+      createdAt
+      author{
+        name
+      }
+    }
+  }
+`
+const TAKE = 10;
 
 function Posts() {
-  return (
-    <div>
-      Posts
-    </div>
-  )
+
+ const [skip,setSkip] = useState(0);
+
+ const {data,loading,error} = useQuery<PostData, QueryInput>(GET_POST,{variables:{skip, take:TAKE}});
+
+ console.log({data, loading, error})
+ 
+ if(loading)
+    return <div>Loading....</div>
+
+  if(error)
+    return <div>{JSON.stringify(error)}</div>
+
+  return <div>Post</div>
+  
 }
 
 export default Posts
