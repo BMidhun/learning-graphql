d5198803251f373dba4abcf9878738818e9ffe57
BMidhun <midhunbabygeorge@gmail.com>
2022-09-05 00:15:06 +0530

    add frontend app

---
+++ social-media-app/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import Posts from './pages/posts'
+import Profile from './pages/profile'
+import SignIn from './pages/signin'
+import SignUp from './pages/signup'
+
+function App() {
+  return <Router>
+            <Routes>
+              <Route path='/posts' element={<Posts />}></Route>
+              <Route path='/signup' element={<SignUp />}></Route>
+              <Route path='/signin' element={<SignIn />}></Route>
+              <Route path='/profile/:id' element={<Profile />}></Route>
+              <Route path="*" element={<Navigate to={"/posts"}/>}></Route>
+            </Routes>
+        </Router>
+}
+
+export default App
