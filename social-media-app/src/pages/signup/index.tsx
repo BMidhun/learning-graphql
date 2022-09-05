7a45980901e35826b151225a1cf07fcb93a3590b
BMidhun <midhunbabygeorge@gmail.com>
2022-09-06 01:55:33 +0530

    add signup mutations

--- social-media-app/src/pages/signup/index.tsx
+++ social-media-app/src/pages/signup/index.tsx
@@ -1,8 +1,39 @@
-import React, { FormEvent, useRef } from 'react'
+import { gql, useMutation } from '@apollo/client';
+import React, { FormEvent, useEffect, useRef, useState } from 'react'
 import Button from '../../components/Button/button'
 import TextInput from '../../components/TextInput/text-input'
 
+interface ISignUpData {
+  errors: { message: string }[];
+  token: string;
+}
+
+interface MutationVariable {
+  credentials: {
+    email: string;
+    password: string;
+  };
+  bio:string,
+  name:string
+}
+
+interface ISignUpResponse {
+  signUp: ISignUpData;
+}
+
+const SIGNUP_ACTION = gql`
+ mutation signUp($credentials: CredentialInput!, $name: String!, $bio: String!) {
+  signUp(credentials: $credentials, name: $name, bio: $bio) {
+      errors{
+        message
+      }
+      token
+  }
+ }
+`;
+
 function SignUp() {
+  const [formError, setFormError] = useState<string | null>(null);
   const formRef = useRef<{ [key: string]: string }>({
     email:"",
     password:"",
@@ -10,14 +41,33 @@ function SignUp() {
     bio:""
   });
 
+  const [postSignUp, { loading, data, error }] = useMutation<
+  ISignUpResponse,
+    MutationVariable
+  >(SIGNUP_ACTION);
+
   function onHandleInput(name:string,value:string) {
     formRef.current[name] = value;
   }
 
   function onSubmit (e:FormEvent) {
     e.preventDefault();
-    alert("Invoked");
+    const {email,password,name,bio} = formRef.current
+    postSignUp({variables:{credentials:{email, password}, bio, name}});
   }
+
+  useEffect(() => {
+    if (data) {
+      if (data.signUp.errors.length) {
+        setFormError(data.signUp.errors[0].message);
+      }
+
+      if (data.signUp.token) {
+        setFormError(null);
+        localStorage.setItem("token", data.signUp.token);
+      }
+    }
+  }, [data]);
 
   return (
     <form onSubmit={onSubmit}>
@@ -33,9 +83,14 @@ function SignUp() {
     <div className='my-2'>
        <TextInput name='bio' initialValue='' placeholder='Enter your bio' onHandleInput={ onHandleInput}  required={true}/>
     </div>
-    <div>
+    <div className='my-2'>
       <Button onClick={() => null}>Sign Up</Button>
     </div>
+    {formError ? (
+        <p className="my-2" style={{ color: "red" }}>
+          {formError}
+        </p>
+      ) : null}
 </form>
   )
 }
